a6ca5c040269c65d01dee3ccfc27518035662554
Casper van Battum <cvanbattum@gmail.com>
2019-02-24 17:29:28 +0100

    Removed Rider IDE folder

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,5 @@
 ﻿using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
-    }
+public class GameManager : MonoBehaviour {
+	
 }
